a6d859f36c774c876993d8539e81d142561c4443
Mark <2017mark@gmail.com>
2021-06-30 21:28:14 -0500

    Removing import from main

--- loader.py
+++ loader.py
@@ -8,14 +8,20 @@ def load_data():
     terran_units = []
 
     with open('./data/protoss_units.csv') as protoss_file:
+        reader = csv.reader(protoss_file)
+        next(reader)
         for line in csv.reader(protoss_file):
             protoss_units.append(Unit(line))
 
     with open('./data/zerg_units.csv') as zerg_file:
+        reader = csv.reader(zerg_file)
+        next(reader)
         for line in csv.reader(zerg_file):
             zerg_units.append(Unit(line))
 
     with open('./data/terran_units.csv') as terran_file:
+        reader = csv.reader(terran_file)
+        next(reader)
         for line in csv.reader(terran_file):
             terran_units.append(Unit(line))
 
