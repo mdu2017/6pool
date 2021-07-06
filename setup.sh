c1d3b05ecd8e9368defbd604f0a74905266da545
Mark <2017mark@gmail.com>
2021-07-06 18:10:31 -0500

    Adding files for deployment

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"2017mark@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = 8501\n\
+" > ~/.streamlit/config.toml
