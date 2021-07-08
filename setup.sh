b2d1ae70527f5ab33e1e5407552bac4faae22c70
Mark <2017mark@gmail.com>
2021-07-08 16:41:39 -0600

    Fixing setup.sh

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@ email = \"2017mark@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
+[theme]\n\
+base = dark\n\
+textColor = #fffbf5\n\
+font = serif\n\
+" > ~/.streamlit/config.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=true\n\
 port = $PORT\n\
-" > ~/.streamlit/config.toml
-
-echo "\
-[theme]\n\
-base=dark\n\
-textColor=#fffbf5\n\
-font=serif\n\
 " > ~/.streamlit/config.toml
